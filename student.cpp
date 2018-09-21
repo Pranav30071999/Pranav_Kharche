4addbd55bf575aec0348e45400b04c120bc922df
Pranav30071999 <40362318+Pranav30071999@users.noreply.github.com>
2018-09-22 01:10:51 +0530

    Add files via upload
    This is hackerrank question of class Student in which i learn how to create various setters and getters function and how to convert data in one string using objects in <sstream> header.

---
+++ student.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <sstream>
+#include <stdlib.h>
+using namespace std;
+
+class Student
+{
+    private:
+    
+    int age;
+    string first_name;
+    string last_name;
+    int standard;
+    
+    public:
+    
+    void set_age(int);
+    void set_standard(int);
+    void set_first_name(string);
+    void set_last_name(string);
+    int get_age();
+    int get_standard();
+    string get_first_name();
+    string get_last_name();
+    string to_string();
+};
+
+void Student::set_age(int age)
+{
+    this->age = age;
+    
+}
+
+void Student::set_standard(int standard)
+{
+    this->standard = standard;
+}
+
+void Student::set_first_name(string s)
+{
+    this->first_name = s;
+}
+
+void Student::set_last_name(string s)
+{
+    this->last_name = s;
+}
+
+int Student::get_age()
+{
+    return age;
+}
+
+int Student::get_standard()
+{
+    return standard;
+}
+
+string Student::get_first_name()
+{
+    return first_name;
+}
+
+string Student::get_last_name()
+{
+    return last_name;
+}
+
+string Student::to_string()
+{
+    stringstream ss;
+    string s3;
+    ss<<age<<','<<first_name<<','<<last_name<<','<<standard;
+    ss>>s3;
+   
+    return s3;
+    
+}
+
+int main() {
+    int age, standard;
+    string first_name, last_name;
+    
+    cin >> age >> first_name >> last_name >> standard;
+    
+    Student st;
+    st.set_age(age);
+    st.set_standard(standard);
+    st.set_first_name(first_name);
+    st.set_last_name(last_name);
+    
+    cout << st.get_age() << "\n";
+    cout << st.get_last_name() << ", " << st.get_first_name() << "\n";
+    cout << st.get_standard() << "\n";
+    cout << "\n";
+    cout << st.to_string();
+    
+    return 0;
+}
